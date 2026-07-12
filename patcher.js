cf6d7b976ae3a624c8b830bc1ec9ad63d8d986f1
JV-60FPS <gaynalgaynal0@gmail.com>
2026-07-12 20:44:07 +0600

    Add patchSharkSampleTableMethod to patcher.js

---
+++ patcher.js
@@ -0,0 +1,9 @@
+async function patchSharkSampleTableMethod(buffer) {
+  // 👉 your custom logic here
+
+  return {
+    output: buffer // must return Buffer or Uint8Array
+  };
+}
+
+module.exports = { patchSharkSampleTableMethod };
